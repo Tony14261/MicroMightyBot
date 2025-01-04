9d552f7ff0c66f07f94b0e8e40cecb0ce992e6b1
Tony14261 <tonyvu4913@gmail.com>
2025-01-04 22:31:42 +0700

    v1.3.10d-SNAPSHOT

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import time
 
 import discord
 from discord.commands import option
-from discord.ext import commands
+
+#from discord.ext import commands
 from dotenv import load_dotenv
 
 from exceptions import InvalidMethod, UnknownError
@@ -108,7 +109,7 @@ intents = discord.Intents()
 intents.messages = True
 intents.message_content = True
 intents.presences = True
-bot = commands.Bot(intents=intents)
+bot = discord.Bot(intents=intents)
 
 #====================Slash commands====================
 
